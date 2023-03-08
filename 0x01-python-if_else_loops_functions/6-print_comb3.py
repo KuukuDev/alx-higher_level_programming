378401804b7194f2d9597d9647ee2986ec9012f7
KuukuDev <owurakubuabeng777@gmail.com>
2023-03-08 17:05:48 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for first_digit in range(0, 10):
         if first_digit == 8 and second_digit == 9:
             print("{}{}".format(first_digit, second_digit))
         else:
-            print("{}{}".format(first_digit,second_digit), end=", ")
+            print("{}{}".format(first_digit, second_digit), end=", ")
